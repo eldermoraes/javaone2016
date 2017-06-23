2c9b48980ecc0fb6874dc32bbbc8820600462d26
Elder Moraes <elder.moraes@gmail.com>
2017-06-22 23:52:50 -0300

    Added -d to docker run

--- build-ocsp17/run.sh
+++ build-ocsp17/run.sh
@@ -3,7 +3,7 @@ docker rm -f host2
 docker rm -f host3
 docker rm -f loadbalancer
 
-docker run --name host1 -p 8080:8080 tomee-war
-docker run --name host2 -p 8081:8080 tomee-war
-docker run --name host3 -p 8082:8080 tomee-war
-docker run --name loadbalancer -p 81:80 --link host1:host1 --link host2:host2 --link host3:host3 --env-file ./env.list jasonwyatt/nginx-loadbalancer
+docker run -d --name host1 -p 8080:8080 tomee-war
+docker run -d --name host2 -p 8081:8080 tomee-war
+docker run -d --name host3 -p 8082:8080 tomee-war
+docker run -d --name loadbalancer -p 81:80 --link host1:host1 --link host2:host2 --link host3:host3 --env-file ./env.list jasonwyatt/nginx-loadbalancer
