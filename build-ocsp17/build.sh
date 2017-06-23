2499b419bd8ae710c9f4893a66a59d92dc41a84a
Elder Moraes <elder.moraes@gmail.com>
2017-06-23 00:19:45 -0300

    Log improved

--- build-ocsp17/build.sh
+++ build-ocsp17/build.sh
@@ -1,13 +1,3 @@
-echo " "
-echo "====================================================="
-echo "Remove a imagem anterior, se houver"
-echo "====================================================="
-docker rmi tomee-war || true
-echo " "
+docker rmi -f tomee-war || true
 
-echo " "
-echo "====================================================="
-echo "Build da nova imagem"
-echo "====================================================="
 docker build -t tomee-war --build-arg WAR_FILE=ocsp17.war .
-echo " "
