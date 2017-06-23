8dc63aea8a569a640a177de83d7ecb9ad833c530
Elder Moraes <elder.moraes@gmail.com>
2017-06-23 00:14:58 -0300

    Log improved

--- build-ocsp17/build.sh
+++ build-ocsp17/build.sh
@@ -1,3 +1,13 @@
+echo " "
+echo "====================================================="
+echo "Remove a imagem anterior, se houver"
+echo "====================================================="
 docker rmi tomee-war || true
+echo " "
 
+echo " "
+echo "====================================================="
+echo "Build da nova imagem"
+echo "====================================================="
 docker build -t tomee-war --build-arg WAR_FILE=ocsp17.war .
+echo " "
--- build-ocsp17/run.sh
+++ build-ocsp17/run.sh
@@ -1,9 +1,25 @@
+echo " "
+echo "====================================================="
+echo "Remove os containers anteriores, se existirem"
+echo "====================================================="
 docker rm -f host1 || true
 docker rm -f host2 || true
 docker rm -f host3 || true
 docker rm -f loadbalancer || true
+echo " "
 
+echo " "
+echo "====================================================="
+echo "Constrói os novos containers"
+echo "====================================================="
 docker run -d --name host1 -p 8080:8080 tomee-war
 docker run -d --name host2 -p 8081:8080 tomee-war
 docker run -d --name host3 -p 8082:8080 tomee-war
 docker run -d --name loadbalancer -p 81:80 --link host1:host1 --link host2:host2 --link host3:host3 --env-file ./env.list jasonwyatt/nginx-loadbalancer
+echo " "
+
+echo " "
+echo "====================================================="
+echo "Containers criado"
+echo "====================================================="
+echo " "
